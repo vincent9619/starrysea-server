d7594a2c575f0242e91a6eeb1f8a67b06ef18336
KumaLoveliver <kuma_loveliver@163.com>
2019-11-17 10:58:27 +0800

    增加文件操作工具类;引入lombak

---
+++ util/src/main/java/top/starrysea/rina/util/exception/RinaException.java
@@ -0,0 +1,12 @@
+package top.starrysea.rina.util.exception;
+
+public class RinaException extends RuntimeException {
+
+    public RinaException(Throwable throwable) {
+        super(throwable);
+    }
+
+    public RinaException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+}

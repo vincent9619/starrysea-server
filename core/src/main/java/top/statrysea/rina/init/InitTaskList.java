e1ff2135dfb941a39e20e42b7def009556df41fd
kuma_loveliver <kuma_loveliver@163.com>
2019-11-18 09:41:37 +0800

    优化线程工具类,只传入无入参无出参的函数对象即可

--- core/src/main/java/top/statrysea/rina/init/InitTaskList.java
+++ core/src/main/java/top/statrysea/rina/init/InitTaskList.java
@@ -21,26 +21,25 @@ public class InitTaskList {
 
     private static InitTask initServerConfigTask = () -> {
         log.info("执行初始化服务器配置的初始化任务");
-	    ServerConfig initConfig = FileUtil.readYamlFile("serverConfig.yaml", ServerConfig.class);
-	    RinaObjectFactory.putRinaObject(ServerConfig.class, initConfig);
-	    log.info("服务器配置初始化完成。将使用以下配置：");
-	    log.info(RinaObjectFactory.getRinaObject(ServerConfig.class).toString());
+        ServerConfig initConfig = FileUtil.readYamlFile("serverConfig.yaml", ServerConfig.class);
+        RinaObjectFactory.putRinaObject(ServerConfig.class, initConfig);
+        log.info("服务器配置初始化完成。将使用以下配置：");
+        log.info(RinaObjectFactory.getRinaObject(ServerConfig.class).toString());
     };
 
     private static InitTask initServerBackgroundTask = () -> {
         log.info("执行初始化服务器后台任务的初始化任务");
 
-	    // 定时刷新服务器设置
-	    ThreadUtil.registerScheduleTask(() -> {
-		    ServerConfig currentConfig = RinaObjectFactory.getRinaObject(ServerConfig.class);
-		    ServerConfig newConfig = FileUtil.readYamlFile("serverConfig.yaml", ServerConfig.class);
-		    if (!newConfig.equals(currentConfig)) {
-			    RinaObjectFactory.putRinaObject(ServerConfig.class, newConfig);
-			    log.info("服务器配置文件变更。新的配置如下：");
-			    log.info(RinaObjectFactory.getRinaObject(ServerConfig.class).toString());
-		    }
-		    return 0;
-	    }, 1, TimeUnit.SECONDS);
+        // 定时刷新服务器设置
+        ThreadUtil.registerScheduleTask(() -> {
+            ServerConfig currentConfig = RinaObjectFactory.getRinaObject(ServerConfig.class);
+            ServerConfig newConfig = FileUtil.readYamlFile("serverConfig.yaml", ServerConfig.class);
+            if (!newConfig.equals(currentConfig)) {
+                RinaObjectFactory.putRinaObject(ServerConfig.class, newConfig);
+                log.info("服务器配置文件变更。新的配置如下：");
+                log.info(RinaObjectFactory.getRinaObject(ServerConfig.class).toString());
+            }
+        }, 1, TimeUnit.SECONDS);
 
     };
 

28d1b2121090e3277d67b4b5c60b452f3eb5d2d6
vincent9619 <gonghao9619@gmail.com>
2019-11-19 21:40:37 +0800

    NIO问题版本Ver1.1

--- core/src/main/java/top/starrysea/rina/core/HttpNIO.java
+++ core/src/main/java/top/starrysea/rina/core/HttpNIO.java
@@ -1,60 +1,65 @@
 package top.starrysea.rina.core;
-import lombok.extern.flogger.Flogger;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.sun.net.httpserver.HttpHandler;
+import lombok.extern.slf4j.Slf4j;
+import top.starrysea.rina.init.ServerConfig;
+import top.starrysea.rina.util.factory.RinaObjectFactory;
 import top.starrysea.rina.util.thread.ThreadUtil;
-
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.*;
-import java.nio.charset.Charset;
-import java.util.Arrays;
 import java.util.Iterator;
-import java.util.List;
-import java.util.concurrent.Executor;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-import java.util.function.Supplier;
-
+@Slf4j
 public class HttpNIO {
-    private static final Logger logger = LoggerFactory.getLogger(HttpHandler.class);
-    public static void ExecuteNio() throws IOException {
+
+    public static void executeNio() {
         // 创建 ServerSocketChannel
-        ServerSocketChannel serverChannel = ServerSocketChannel.open();
-        serverChannel.bind(new InetSocketAddress(8888));
+        try {ServerSocketChannel serverChannel = ServerSocketChannel.open();
 
-        // 配置为非阻塞
-        serverChannel.configureBlocking(false);
-        // 注册选择器
-        Selector selector = Selector.open();
-        serverChannel.register(selector, SelectionKey.OP_ACCEPT);
-        // 创建线程池
-        ThreadUtil ex = new ThreadUtil();
-        //Executor ex = new ThreadPoolExecutor(8, 8, 500, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
-        // 创建处理器
-        while (true) {
-            // 每次等待0.5秒，0.5秒后返回的就绪通道为0则继续阻塞
-            if (selector.select(500) == 0) {
-                continue;
-            }
-            // 获取待处理的seletionKey
-            Iterator<SelectionKey> it = selector.selectedKeys().iterator();
-            while (it.hasNext()) {
-                SelectionKey key = it.next();
-                HttpHandler httpHandler = new HttpHandler(key);
-                //ex.exec(httpHandler);
-                //ThreadUtil.exec(httpHandler);
-                logger.info("Link Start");
-                ThreadUtil.exec(httpHandler::run);
-                logger.info("Link End");
+            serverChannel.bind(new InetSocketAddress(RinaObjectFactory.getRinaObject(ServerConfig.class).getPort()));
 
+            // 配置为非阻塞
+
+            serverChannel.configureBlocking(false);
+
+            // 注册选择器
+            Selector selector = Selector.open();
+
+            serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+            // 创建线程池
+            ThreadUtil ex = new ThreadUtil();
+            //Executor ex = new ThreadPoolExecutor(8, 8, 500, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
+            // 创建处理器
+
+            while (true) {
+                // 每次等待0.5秒，0.5秒后返回的就绪通道为0则继续阻塞
+                try {
+                    if (selector.select(500) == 0) {
+                        continue;
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                // 获取待处理的selectionKey
+                Iterator<SelectionKey> it = selector.selectedKeys().iterator();
+                while (it.hasNext()) {
+                    SelectionKey key = it.next();
+                    HttpHandler httpHandler = new HttpHandler(key);
+                    log.info("开始建立与客户端连接");
+                    ThreadUtil.exec(httpHandler::run);
+                    log.info("结束与客户端连接");
+
+                }
             }
+        } catch (ClosedChannelException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
+        // 处理请求
 
-    // 处理请求
     private static class HttpHandler implements Runnable {
         // TCP包长度
         private int bufferSize = 1024;
@@ -64,7 +69,8 @@ public class HttpNIO {
         private SelectionKey key;
 
 
-        //@org.jetbrains.annotations.Contract(pure = true)
+
+
         public HttpHandler(SelectionKey key) {
             this.key = key;
         }
@@ -85,17 +91,20 @@ public class HttpNIO {
         // socketChannel 读取数据
         public void handleRead() throws Exception {
             // 获取channel
-            SocketChannel channel = (SocketChannel) key.channel();
+
+            try(SocketChannel channel = (SocketChannel) key.channel()){;
             // 获取Buffer并重置
             ByteBuffer buffer = (ByteBuffer) key.attachment();
             buffer.clear();
             // 从通道获取内容
+
             if (channel.read(buffer) == -1) {
-                logger.info("Link Failed");
+                log.info("连接失败");
                 channel.close();
             } else {
-                logger.info("Link Succeed");
+                log.info("连接成功");
                 channel.close();
+            }
             }
         }
 
--- core/src/main/java/top/starrysea/rina/core/Rina.java
+++ core/src/main/java/top/starrysea/rina/core/Rina.java
@@ -6,17 +6,18 @@ import top.starrysea.rina.util.factory.RinaObjectFactory;
 import top.starrysea.rina.init.InitTaskList;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 
 @Slf4j
 public class Rina {
 
     private static boolean isStart = false;
 
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
         iku();
     }
 
-    public static void iku() throws IOException {
+    public static void iku() throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         try {
             InitTaskList initTaskList = RinaObjectFactory.generateRinaObject(InitTaskList.class);
             initTaskList.execute();
@@ -26,8 +27,8 @@ public class Rina {
         isStart = true;
         while (isStart) {
             //TODO 运行的代码
-            HttpNIO.ExecuteNio();
-
+            RinaObjectFactory.generateRinaObject( HttpNIO.class);
+            HttpNIO.executeNio();
         }
     }
 

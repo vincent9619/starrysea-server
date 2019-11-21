88afbd15cb583dcce76b8af77166132eb542fe6e
kuma_loveliver <kuma_loveliver@163.com>
2019-11-21 16:59:11 +0800

    修复一下HTTPNIO类

--- core/src/main/java/top/starrysea/rina/core/HttpNIO.java
+++ core/src/main/java/top/starrysea/rina/core/HttpNIO.java
@@ -1,141 +1,95 @@
 package top.starrysea.rina.core;
+
+import lombok.extern.slf4j.Slf4j;
+import top.starrysea.rina.init.ServerConfig;
+import top.starrysea.rina.util.factory.RinaObjectFactory;
 import top.starrysea.rina.util.thread.ThreadUtil;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.*;
-import java.nio.charset.Charset;
-import java.util.Arrays;
 import java.util.Iterator;
-import java.util.List;
-import java.util.concurrent.Executor;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-import java.util.function.Supplier;
 
+
+@Slf4j
 public class HttpNIO {
+    private static boolean isStart = false;
 
-    public static void ExecuteNio() throws IOException {
-        // 创建 ServerSocketChannel
-        ServerSocketChannel serverChannel = ServerSocketChannel.open();
-        serverChannel.bind(new InetSocketAddress(9999));
-        // 配置为非阻塞
-        serverChannel.configureBlocking(false);
-        // 注册选择器
-        Selector selector = Selector.open();
-        serverChannel.register(selector, SelectionKey.OP_ACCEPT);
-        // 创建线程池
-        ThreadUtil ex = new ThreadUtil();
-        //Executor ex = new ThreadPoolExecutor(8, 8, 500, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
-        // 创建处理器
-        while (true) {
-            // 每次等待0.5秒，0.5秒后返回的就绪通道为0则继续阻塞
-            if (selector.select(500) == 0) {
-                continue;
-            }
-            // 获取待处理的seletionKey
-            Iterator<SelectionKey> it = selector.selectedKeys().iterator();
-            while (it.hasNext()) {
-                SelectionKey key = it.next();
-                HttpHandler httpHandler = new HttpHandler(key);
-                //ex.exec(httpHandler);
-                //ThreadUtil.exec(httpHandler);
-                ThreadUtil.exec(httpHandler::run);
+    public static void yame() {
+        isStart = false;
+    }
 
+    public void executeNio() {
+        ServerConfig serverConfig = RinaObjectFactory.getRinaObject(ServerConfig.class);
+        try (ServerSocketChannel serverChannel = ServerSocketChannel.open();
+             Selector selector = Selector.open()) {
+            serverChannel.bind(new InetSocketAddress(serverConfig.getPort()));
+            serverChannel.configureBlocking(false);
+            serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+            isStart = true;
+            while (isStart) {
+                if (selector.select(serverConfig.getWaitTime()) == 0) {
+                    continue;
+                }
+                // 获取待处理的selectionKey
+                Iterator<SelectionKey> it = selector.selectedKeys().iterator();
+                while (it.hasNext()) {
+                    SelectionKey Key = it.next();
+                    log.info("开始建立与客户端连接");
+                    ThreadUtil.exec(() -> httpHandler(Key));
+                    log.info("结束与客户端连接");
+                    it.remove();
+                }
             }
+        } catch (ClosedChannelException e) {
+            log.error(e.getMessage(), e);
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
         }
     }
 
     // 处理请求
-    private static class HttpHandler implements Runnable {
+
+    private void httpHandler(SelectionKey key) {
         // TCP包长度
-        private int bufferSize = 1024;
+        int bufferSize = 1024;
         // 编码字符集
-        private String charset = "UTF-8";
-        // 注册的事件
-        private SelectionKey key;
-
-        //@org.jetbrains.annotations.Contract(pure = true)
-        public HttpHandler(SelectionKey key) {
-            this.key = key;
-        }
-
-        // 处理ServerSocketChannel接受请求
-        public void handleAccept() throws Exception {
+        String charset = "UTF-8";
+        if (key.isAcceptable()) {
             ServerSocketChannel serverChannel = (ServerSocketChannel) key.channel();
             // 接收的客户端的连接
-
-            SocketChannel channel = serverChannel.accept();
-            channel.configureBlocking(false);
-            // 注册到选择器
-            channel.register(key.selector(), SelectionKey.OP_READ,
-                    ByteBuffer.allocate(bufferSize));
-            //channel.close();
-        }
-
-        // socketChannel 读取数据
-        public void handleRead() throws Exception {
-            // 获取channel
-            SocketChannel channel = (SocketChannel) key.channel();
-            // 获取Buffer并重置
-            ByteBuffer buffer = (ByteBuffer) key.attachment();
-            buffer.clear();
-            // 从通道获取内容
-            if (channel.read(buffer) == -1) {
-                channel.close();
-            } else {
-                // 接受请求数据
-                buffer.flip();
-                String reciveMsg = Charset.forName(charset).newDecoder().decode(buffer).toString();
-                List<String> requestContent = Arrays.asList(reciveMsg.split("\r\n"));
-
-
-                for(int i=0; i<requestContent.size() ;i++){
-                    System.out.println(requestContent.get(i));
-                }
-
-
-
-                // 返回客户端
-                StringBuilder sendMsg = new StringBuilder();
-                sendMsg.append("HTTP/1.1 200 OK\r\n");// 响应行
-                // 响应头
-                sendMsg.append("cache-control: private;\r\n")
-                        .append("content-type: text/html; charset=utf-8\r\n")
-                        .append("\r\n")
-                        // 响应体
-                        .append("<!DOCTYPE html><html lang=\"zh-cn\">")
-                        .append("<head><meta charset=\"utf-8\"/><title>测试HttpServer</title></head>")
-                        .append("<body><h3>服务端接收到的请求报文</h3>");
-                for (String line : requestContent) {
-                    sendMsg.append(line+"</br>");
-                    if (line.isEmpty()) {
-                        break;
-                    }
-                }
-                sendMsg.append("</body>");
-                buffer = ByteBuffer.wrap(sendMsg.toString().getBytes(charset));
-                // 发送
-                channel.write(buffer);
-                channel.close();
-            }
-        }
-
-        @Override
-        public void run() {
             try {
-                if (key.isAcceptable()) {
-                    handleAccept();
+                SocketChannel channel = serverChannel.accept();
+                if (channel == null) {
+                    return;
                 }
-                if (key.isReadable()) {
-                    handleRead();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
+                channel.configureBlocking(false);
+                // 注册到选择器
+                channel.register(key.selector(), SelectionKey.OP_READ,
+                        ByteBuffer.allocate(bufferSize));
+            } catch (ClosedChannelException e) {
+                log.error(e.getMessage(), e);
+            } catch (IOException e) {
+                log.error(e.getMessage(), e);
             }
         }
-
+        if (key.isReadable()) {
+            try (SocketChannel channel = (SocketChannel) key.channel()) {
+                // 获取Buffer并重置
+                ByteBuffer buffer = (ByteBuffer) key.attachment();
+                buffer.clear();
+                // 从通道获取内容
+                if (channel.read(buffer) == -1) {
+                    log.info("连接失败");
+                } else {
+                    log.info("连接成功");
+                }
+            } catch (IOException ex) {
+                log.error(ex.getMessage(), ex);
+            }
+        }
     }
 }
+
+

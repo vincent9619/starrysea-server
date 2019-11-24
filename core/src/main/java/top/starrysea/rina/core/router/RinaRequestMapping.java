6c2fc937deddd70124b39e2a2f07ece06b0be2b5
Shuhuai Cao <csh980717@outlook.com>
2019-11-24 21:44:00 +0800

    将对map操作放入方法中

--- core/src/main/java/top/starrysea/rina/core/router/RinaRequestMapping.java
+++ core/src/main/java/top/starrysea/rina/core/router/RinaRequestMapping.java
@@ -2,9 +2,18 @@ package top.starrysea.rina.core.router;
 
 import lombok.Data;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Data
 public class RinaRequestMapping {
-	private Map<Request, RinaRequestRouteInfo> routeInfoMap;
+	private Map<RequestInfo, RinaRequestRouteInfo> routeInfoMap = new HashMap<>();
+
+	public void registerRouteInfo(RequestInfo requestInfo, RinaRequestRouteInfo routeInfo){
+		routeInfoMap.put(requestInfo, routeInfo);
+	}
+
+	public RinaRequestRouteInfo getRouteInfo(RequestInfo requestInfo){
+		return routeInfoMap.get(requestInfo);
+	}
 }

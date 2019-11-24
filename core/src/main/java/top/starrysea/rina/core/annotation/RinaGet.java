db999fb0bf12319476587da1074b432a2a80e5a2
Shuhuai Cao <csh980717@outlook.com>
2019-11-24 20:33:54 +0800

    添加请求路由相关注解

---
+++ core/src/main/java/top/starrysea/rina/core/annotation/RinaGet.java
@@ -0,0 +1,10 @@
+package top.starrysea.rina.core.annotation;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+public @interface RinaGet {
+	String value();
+}

8bc40d9f5daa498bf0c96aa79c0236af163728ff
Shuhuai Cao <csh980717@outlook.com>
2019-11-27 12:23:33 +0800

    为RequestInfo添加全参数构造器

--- core/src/main/java/top/starrysea/rina/core/router/RequestInfo.java
+++ core/src/main/java/top/starrysea/rina/core/router/RequestInfo.java
@@ -1,9 +1,13 @@
 package top.starrysea.rina.core.router;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import top.starrysea.rina.core.connection.entity.enums.HttpMethod;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RequestInfo {
 	private HttpMethod httpMethod;
 	private String path;

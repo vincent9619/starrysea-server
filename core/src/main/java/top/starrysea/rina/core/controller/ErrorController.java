3219b6b6a51bdd2e05b3853e44b759125038a687
Shuhuai Cao <csh980717@outlook.com>
2019-11-29 12:25:29 +0800

    添加错误页面html

--- core/src/main/java/top/starrysea/rina/core/controller/ErrorController.java
+++ core/src/main/java/top/starrysea/rina/core/controller/ErrorController.java
@@ -6,7 +6,21 @@ import top.starrysea.rina.core.annotation.RinaGet;
 @RinaController
 public class ErrorController {
 	@RinaGet("/error/404")
-	public void notFound(){
-
+	public String notFound(){
+		return "<html lang=\"zh\">" +
+				"<head>" +
+				"<title>出错了zura</title>" +
+				"</head>" +
+				"<body>" +
+				"<div>" +
+				"<div style=\"text-align:center\">" +
+				"<h1>404</h1>" +
+				"<p>无法找到您请求的资源</p>" +
+				"<hr>" +
+				"<p>Rina :(</p>" +
+				"</div>" +
+				"</div>" +
+				"</body>" +
+				"</html>";
 	}
 }

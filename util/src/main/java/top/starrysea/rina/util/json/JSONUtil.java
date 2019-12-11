42749435549fcf1eb83e4ed9be01b5a1d0b480cf
kuma_loveliver <kuma_loveliver@163.com>
2019-12-11 14:46:58 +0800

    json工具类增加一个json字符串转map的方法

--- util/src/main/java/top/starrysea/rina/util/json/JSONUtil.java
+++ util/src/main/java/top/starrysea/rina/util/json/JSONUtil.java
@@ -1,9 +1,13 @@
 package top.starrysea.rina.util.json;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import top.starrysea.rina.util.exception.RinaException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 public class JSONUtil {
@@ -29,6 +33,15 @@ public class JSONUtil {
     public static void prettyPrint(Object object) {
         try {
             log.info(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(object));
+        } catch (JsonProcessingException e) {
+            throw new RinaException(e.getMessage(), e);
+        }
+    }
+
+    public static Map<String, Object> toMap(String jsonStr) {
+        try {
+            return objectMapper.readValue(jsonStr, new TypeReference<HashMap<String, Object>>() {
+            });
         } catch (JsonProcessingException e) {
             throw new RinaException(e.getMessage(), e);
         }

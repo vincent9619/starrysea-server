f9e50f2cd71a4cbf983c9347d3cdcd89bdd1ae0f
Shuhuai Cao <csh980717@outlook.com>
2019-12-24 22:35:32 +0800

    简单的连接池

---
+++ jdbc/src/main/java/top/starrysea/rina/jdbc/cp/ConnectionPool.java
@@ -0,0 +1,16 @@
+package top.starrysea.rina.jdbc.cp;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface ConnectionPool {
+	Connection getConnection() throws SQLException;
+
+	boolean releaseConnection(Connection connection);
+
+	String getUrl();
+
+	String getUser();
+
+	String getPassword();
+}
